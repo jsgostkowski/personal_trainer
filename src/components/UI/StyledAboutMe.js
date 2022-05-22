b63257871658fc802eb77fc9c205aa8feec1fdf9
jgostkowski <gostkowskijakub@gmail.com>
2022-05-22 15:56:16 +0200

    adding about me section and styled component

---
+++ src/components/UI/StyledAboutMe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const SectionAboutMe = styled.div`
+  display: flex;
+  height: 600px;
+
+  margin-top: 15px;
+`;
+
+export const StyledAboutMe = () => {
+  return <SectionAboutMe />;
+};
